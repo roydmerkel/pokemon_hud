6b2a4711dc79f5aa0e5c185a9ca02c6be8a98915
Roy Merkel <roy_d_merkel@hotmail.com>
2024-07-13 00:28:35 -0700

    Added polyfills for eventual possible IE support.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
+require('es5-shim');
+require('es5-shim/es5-sham');
+require('console-polyfill');
+require('es6-promise');
+require('fetch-ie8');
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+//import App from './App';
+let App = null;
+if (window.fetch) {
+  // Check whether ES6 is supported in Modern Browsers
+  const module = await import("./App");
+  App = module.default;
+} else {
+  // For legacy or old browsers
+  await import("./polyfills");
+  const module = await import("./App");
+  App = module.default;
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
